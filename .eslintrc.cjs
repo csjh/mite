2ea9fae037fc1a3b518661ce49be287559a394e1
csjh <61282104+csjh@users.noreply.github.com>
2024-05-09 02:03:07 -0400

    fix eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
         browser: true,
         es2021: true
     },
-    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
     overrides: [
         {
             env: {
@@ -20,11 +20,5 @@ module.exports = {
         ecmaVersion: "latest",
         sourceType: "module"
     },
-    plugins: ["@typescript-eslint"],
-    rules: {
-        indent: ["error", 4],
-        "linebreak-style": ["error", "unix"],
-        quotes: ["error", "double"],
-        semi: ["error", "always"]
-    }
+    plugins: ["@typescript-eslint"]
 };
