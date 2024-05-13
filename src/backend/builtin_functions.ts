16b396c30927b08f75387de966b716002d314a1d
csjh <61282104+csjh@users.noreply.github.com>
2024-05-12 22:49:32 -0400

    ctx.mod -> mod in builtin_functions

--- src/backend/builtin_functions.ts
+++ src/backend/builtin_functions.ts
@@ -4,75 +4,70 @@ import { ARENA_HEAP_OFFSET as AHO, ARENA_HEAP_POINTER as AHP } from "./utils.js"
 
 const PAGE_SIZE = 65536;
 
-function enhanced_global(ctx: Context, name: string) {
+function enhanced_global(mod: binaryen.Module, name: string) {
     return {
         get() {
-            return ctx.mod.global.get(name, binaryen.i32);
+            return mod.global.get(name, binaryen.i32);
         },
         set(val: number) {
-            return ctx.mod.global.set(name, val);
+            return mod.global.set(name, val);
         }
     };
 }
 
-function enhanced_local(ctx: Context, idx: number) {
+function enhanced_local(mod: binaryen.Module, idx: number) {
     return {
         get() {
-            return ctx.mod.local.get(idx, binaryen.i32);
+            return mod.local.get(idx, binaryen.i32);
         },
         set(val: number) {
-            return ctx.mod.local.set(idx, val);
+            return mod.local.set(idx, val);
         },
         tee(val: number) {
-            return ctx.mod.local.tee(idx, val, binaryen.i32);
+            return mod.local.tee(idx, val, binaryen.i32);
         },
         deref: {
             get() {
-                return ctx.mod.i32.load(0, 0, ctx.mod.local.get(idx, binaryen.i32), "main_memory");
+                return mod.i32.load(0, 0, mod.local.get(idx, binaryen.i32), "main_memory");
             },
             set(val: number) {
-                return ctx.mod.i32.store(
-                    0,
-                    0,
-                    ctx.mod.local.get(idx, binaryen.i32),
-                    val,
-                    "main_memory"
-                );
+                return mod.i32.store(0, 0, mod.local.get(idx, binaryen.i32), val, "main_memory");
             }
         }
     };
 }
 
 export function addBuiltins(ctx: Context) {
-    const i32 = ctx.mod.i32;
+    const mod = ctx.mod;
+    const i32 = mod.i32;
 
-    const ARENA_HEAP_OFFSET = enhanced_global(ctx, AHO);
-    const ARENA_HEAP_POINTER = enhanced_global(ctx, AHP);
+    const ARENA_HEAP_OFFSET = enhanced_global(mod, AHO);
+    const ARENA_HEAP_POINTER = enhanced_global(mod, AHP);
 
-    const DESIRED_SIZE = enhanced_local(ctx, 0);
+    const DESIRED_SIZE = enhanced_local(mod, 0);
     // prettier-ignore
-    const arena_heap_malloc = ctx.mod.block(null, [
-        ctx.mod.if(
+    const arena_heap_malloc = mod.block(null, [
+        mod.if(
             i32.ge_u(
                 i32.add(DESIRED_SIZE.get(), i32.add(ARENA_HEAP_OFFSET.get(), ARENA_HEAP_POINTER.get())),
-                i32.mul(ctx.mod.memory.size("main_memory"), i32.const(PAGE_SIZE))),
-            ctx.mod.memory.grow(ctx.mod.memory.size("main_memory"), "main_memory")),
+                i32.mul(mod.memory.size("main_memory"), i32.const(PAGE_SIZE))),
+            mod.memory.grow(mod.memory.size("main_memory"), "main_memory")),
 
         ARENA_HEAP_OFFSET.set(
             i32.add(DESIRED_SIZE.get(), DESIRED_SIZE.tee(ARENA_HEAP_OFFSET.get()))),
 
         i32.add(DESIRED_SIZE.get(), ARENA_HEAP_POINTER.get())
     ]);
-    ctx.mod.addFunction(
+    mod.addFunction(
         "arena_heap_malloc",
         binaryen.createType([binaryen.i32]),
         binaryen.i32,
         [],
         arena_heap_malloc
     );
-    ctx.mod.addFunctionExport("arena_heap_malloc", "$arena_heap_malloc");
+    mod.addFunctionExport("arena_heap_malloc", "$arena_heap_malloc");
 
-    const arena_heap_reset = ctx.mod.block(null, [ARENA_HEAP_OFFSET.set(i32.const(0))]);
-    ctx.mod.addFunction("arena_heap_reset", binaryen.none, binaryen.none, [], arena_heap_reset);
-    ctx.mod.addFunctionExport("arena_heap_reset", "$arena_heap_reset");
+    const arena_heap_reset = mod.block(null, [ARENA_HEAP_OFFSET.set(i32.const(0))]);
+    mod.addFunction("arena_heap_reset", binaryen.none, binaryen.none, [], arena_heap_reset);
+    mod.addFunctionExport("arena_heap_reset", "$arena_heap_reset");
 }
