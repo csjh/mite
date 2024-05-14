aefbf34fc6cce76c21229d03e1fcf7e8b7f96ea5
csjh <61282104+csjh@users.noreply.github.com>
2024-05-13 20:58:46 -0400

    add js and dts generation to index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,35 @@ import { compile } from "./compiler.js";
 const program = await fs.readFile(process.argv[2], "utf8");
 
 console.log(compile(program, { as: "wat", optimize: false }));
+console.log(
+    compile(program, {
+        as: "javascript",
+        createInstance(imports) {
+            return {
+                instantiation: `WebAssembly.instantiate(Uint8Array.from(atob("atea"), (c) => c.charCodeAt(0)), ${imports})`
+            };
+        }
+    })
+);
+console.log(compile(program, { as: "dts" }));
 
 const output = compile(program);
 
 await fs.writeFile("out.wasm", output);
 
 const compiled = new WebAssembly.Module(output);
-// @ts-expect-error this works
-const instance = new WebAssembly.Instance(compiled, { console });
+
+// prettier-ignore
+const instance = new WebAssembly.Instance(compiled, {
+    // @ts-expect-error this works
+    console,
+    $mite: new Proxy({}, {
+        get(_, prop) {
+            // @ts-expect-error this works
+            if (prop.startsWith("wrap_")) { return function (ptr, ...args) { return $funcs[ptr](...args); }; }
+        }
+    })
+});
 
 // @ts-expect-error this works
 console.log(instance.exports.main());
