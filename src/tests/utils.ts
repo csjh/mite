d72e1c6de3be18f7a72b81a1b92dfc0c1fd3587a
csjh <61282104+csjh@users.noreply.github.com>
2024-06-03 22:37:40 -0400

    add `getSingleModule` util

--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,6 +1,4 @@
-import { writeFileSync } from "node:fs";
-import { compile } from "../compiler.js";
-import assert from "node:assert";
+import { readFile } from "fs/promises";
 import type {
     AssignmentExpression,
     SequenceExpression,
@@ -12,55 +10,8 @@ import type {
 } from "estree";
 import { ImportSpecifier, parse } from "acorn";
 import { print } from "esrap";
-
-export async function compileAndRun(
-    program: string,
-    expected_output: string[] | number | bigint | "error",
-    func: string = "main"
-) {
-    const compiled = await compile(program, {
-        resolveImport: async () => ({
-            isMite: false,
-            absolute: "",
-            code: ""
-        })
-    });
-    // console.log(compile(program, { as: "wat" }));
-
-    const module = new WebAssembly.Module(compiled);
-
-    writeFileSync("out.wasm", compiled);
-
-    const string_array_output: string[] = [];
-    const instance = new WebAssembly.Instance(module, {
-        console: {
-            log: (x: number) => string_array_output.push(String(x))
-        },
-        $mite: {
-            $memory: new WebAssembly.Memory({ initial: 256 }),
-            $table: new WebAssembly.Table({ initial: 64, element: "anyfunc" }),
-            $heap_pointer: new WebAssembly.Global({ value: "i32", mutable: false }, 0),
-            $heap_offset: new WebAssembly.Global({ value: "i32", mutable: true }, 0),
-            $fn_ptrs_start: new WebAssembly.Global({ value: "i32", mutable: false }, 0),
-            $update_dataview: () => {}
-        }
-    });
-
-    if (expected_output === "error") {
-        // @ts-expect-error wasm exports
-        assert.throws(() => instance.exports[func]());
-        return;
-    }
-
-    // @ts-expect-error wasm exports
-    const number = instance.exports[func]();
-
-    if (typeof expected_output === "number" || typeof expected_output === "bigint") {
-        assert.strictEqual(number, expected_output);
-    } else {
-        assert.deepStrictEqual(string_array_output, expected_output);
-    }
-}
+import { compile } from "../compiler.js";
+import { mite_shared } from "../plugins/shared.js";
 
 function adaptImportsExports(source: string): string {
     const s = { start: 0, end: 0 };
@@ -197,4 +148,28 @@ export function getModule(
 ): Promise<Record<string, (...args: unknown[]) => unknown>> {
     const adapted_for_function = adaptImportsExports(source);
     return new AsyncFunction("$$imports", adapted_for_function)(imports);
+}
+
+export async function getSingleModule(
+    filename: string
+): Promise<Record<string, (...args: unknown[]) => unknown>> {
+    const source = await readFile(filename, "utf-8");
+
+    const bytes = await compile(source, {
+        as: "wasm",
+        resolveImport: async (path) => ({ code: "", isMite: false, absolute: path })
+    });
+
+    const boilerplate = await compile(source, {
+        as: "javascript",
+        createInstance(imports) {
+            return {
+                instantiation: `WebAssembly.instantiate(Uint8Array.from(atob("${Buffer.from(bytes).toString("base64")}"), (c) => c.charCodeAt(0)), ${imports})`
+            };
+        }
+    });
+
+    return getModule(boilerplate, {
+        "mite:shared": await getModule(mite_shared)
+    });
 }
